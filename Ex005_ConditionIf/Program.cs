8432d860f373482aeb9d2f628c270618f870d5c1
AlekseyLavr <govola77@gmail.com>
2023-07-09 21:29:25 +0300

    Ex 005 Condition If

---
+++ Ex005_ConditionIf/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username. == "Masha")
+{
+    Console.WriteLine("Ура, это же MASHA!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
