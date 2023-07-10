1524a2cd5137f5c6840efa75eda7e007d243f4d1
AlekseyLavr <govola77@gmail.com>
2023-07-10 21:14:06 +0300

    Ex008_Array

---
+++ Ex008_Array/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array = {1, 12, 33, 81, 52, 6, 27, 81, 19};
+int n = array.Length;
+int find = 81;
+int i = 0;
+while (i < n)
+{
+    if (array[i] == find)
+    {
+       Console.WriteLine(i);
+       break; 
+    }
+    i++;
+}
