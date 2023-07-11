d8991855bc7665659a1d8ba23c3b91ba19abde09
AlekseyLavr <govola77@gmail.com>
2023-07-11 20:05:24 +0300

    Ex_S2_Square

---
+++ Ex_S2_Square/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 * n1 == n2 || n2 * n2 == n1)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
