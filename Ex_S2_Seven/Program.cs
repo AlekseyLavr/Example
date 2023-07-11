8a35b2169852f97744e0babbdf237374ea37f1bd
AlekseyLavr <govola77@gmail.com>
2023-07-11 19:48:06 +0300

    Ex_S2_Seven

---
+++ Ex_S2_Seven/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 7 == 0 && n % 23 == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
