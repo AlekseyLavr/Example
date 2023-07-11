c83cffba473a84693483c45ae105baff97e85161
AlekseyLavr <govola77@gmail.com>
2023-07-11 20:38:16 +0300

    Ex_002_Number

---
+++ Ex_S2_Number/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+int f(int n)
+{
+    int n1 = n / 10;
+    int n2 = n % 10;
+    if (n1 > n2)
+        return n1;
+    return n2;        
+}
+
+int n = new Random().Next(10, 100);
+Console.WriteLine(n);
+Console.WriteLine(f(n));
