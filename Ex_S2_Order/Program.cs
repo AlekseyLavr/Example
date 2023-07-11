d91e56b0445c7af9ecf4be66eaa4d41e14e2de74
AlekseyLavr <govola77@gmail.com>
2023-07-11 20:12:57 +0300

    Ex_S2_Order

---
+++ Ex_S2_Order/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 % n2 == 0)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine($"Не кратно, остаток: {n1 % n2}");
+}
