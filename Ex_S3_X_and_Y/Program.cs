d7bde735ede722daa6259f656d789c2f09a9e404
AlekseyLavr <govola77@gmail.com>
2023-07-14 20:31:36 +0300

    Ex_S3_X_and_Y

---
+++ Ex_S3_X_and_Y/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите номер четверти: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 4)
+{
+    Console.Write("Введите номер четверти: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1)
+    Console.WriteLine("x > 0 and y > 0");
+else if (n == 2)
+    Console.WriteLine("x > 0 and y < 0");
+else if(n == 3)
+    Console.WriteLine("x < 0 and y < 0");
+else 
+    Console.WriteLine("x < 0 and y > 0");
+
+    
