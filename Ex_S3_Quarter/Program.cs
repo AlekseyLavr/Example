bd8c93a0f3cc88e20d4452eec560bcc2842cf747
AlekseyLavr <govola77@gmail.com>
2023-07-14 20:16:39 +0300

    Ex_S3_Quarter

---
+++ Ex_S3_Quarter/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("x= ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("y= ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+    Console.WriteLine("I");
+else if (x < 0 && y > 0)
+    Console.WriteLine("II");
+else if (x < 0 && y < 0)
+    Console.WriteLine("III");
+else if (x > 0 && y < 0)
+    Console.WriteLine("IV");   
